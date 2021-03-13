40bb4f871c8d8d36caab32fb59633f77105f492b
lrnholanda <lrnholanda@gmail.com>
2021-03-13 21:05:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#lista 1
+users = ['admin','lore','chris', 'ju', 'italo','rafael']
+if users:
+  for user in users:
+    if user == 'admin':
+      print("Hello admin, would you like to see a status report?")
+    else:
+      print(f"Hello {user.title()}, thank you for logging in again!") 
+
+else:
+      print("We need to find some users!"
+
+      
+
